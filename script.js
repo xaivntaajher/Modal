6aac004dfafe71b076ff34e1ffe066d29d04f3f5
xaivntaajher <dn_her@yahoo.com>
2025-01-22 10:15:51 -0600

    create variables for classes and test to the console

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent); // {} not needed, only one line of code is being executed
